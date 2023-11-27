000d30af781ead32a6c2a57bf5428ad20b610341
이윤주 <2020017065@hanyang.ac.kr>
2023-11-27 13:46:30 +0900

    mission7

---
+++ src/main/java/umc/study/domain/Review.java
@@ -0,0 +1,29 @@
+package umc.study.domain;
+
+import lombok.*;
+import umc.study.domain.common.BaseEntity;
+
+import javax.persistence.*;
+
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false, length = 20)
+    private String title;
+
+    private Float score;
+
+}
