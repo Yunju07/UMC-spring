feeb0713d538f88a503de74758fdbfb7e64baaef
이윤주 <2020017065@hanyang.ac.kr>
2023-11-27 15:51:58 +0900

    mission8

---
+++ src/main/java/umc/study/apiPayload/code/ReasonDTO.java
@@ -0,0 +1,19 @@
+package umc.study.apiPayload.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReasonDTO {
+
+    private HttpStatus httpStatus;
+    private String code;
+    private String message;
+    private boolean isSuccess;
+}
